0ddff22defbb512f520b6b0be5968be848393b41
Nimesha-Premaraja <prem.nimesha@prodoscore.com>
2026-04-26 10:47:07 +0530

    private-instance configs

--- compute/main.tf
+++ compute/main.tf
@@ -12,6 +12,20 @@ module "public-instance" {
 
 }
 
+module "private-instance" {
+    depends_on = [ aws_key_pair.dev-key ]
+    source  = "terraform-aws-modules/ec2-instance/aws"
+    version = "6.4.0"
+
+    name = "dev-private-instance"
+    ami = "ami-0ec10929233384c7f"
+    associate_public_ip_address = true
+    availability_zone = "us-east-1b"
+    instance_type   = "t3.micro"
+    key_name = aws_key_pair.dev-key.key_name
+
+}
+
 resource "tls_private_key" "dev-rsa-key" {
   algorithm = "RSA"
   rsa_bits  = 4096
