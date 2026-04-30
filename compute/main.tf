30e42a96a4aff6a5188d608993f7175bf2ef43ae
Nimesha-Premaraja <prem.nimesha@prodoscore.com>
2026-04-30 13:30:39 +0530

    fix: update security group and subnet configurations for public and private instances

--- compute/main.tf
+++ compute/main.tf
@@ -9,6 +9,8 @@ module "public-instance" {
     availability_zone = "us-east-1b"
     instance_type   = "t3.micro"
     key_name = aws_key_pair.dev-key.key_name
+    subnet_id = module.dev_vpc.public_subnet_id
+    vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
 }
 
@@ -23,6 +25,8 @@ module "private-instance" {
     availability_zone = "us-east-1b"
     instance_type   = "t3.micro"
     key_name = aws_key_pair.dev-key.key_name
+    subnet_id = module.dev_vpc.private_subnet_id
+    vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
 }
 
--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -2,6 +2,14 @@ output "vpc_id" {
   value = aws_vpc.this.id
 }
 
+output "vpc_cidr_block" {
+  value = aws_vpc.this.cidr_block
+}
+
+output "vpc_ipv6_cidr_block" {
+  value = aws_vpc.this.ipv6_cidr_block
+}
+
 output "public_subnet_id" {
   value = aws_subnet.public.id
 }
